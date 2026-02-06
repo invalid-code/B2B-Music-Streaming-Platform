86931b7d407a8839ad8e84b457399f52900e85e1
invalid-code <jessgaming55@gmail.com>
2026-02-06 11:17:37 +0800

    removed files that shouldn't be in upstream

--- start-frontend.ps1
+++
@@ -1,4 +0,0 @@
-$Host.UI.RawUI.WindowTitle = "Frontend Dev Server"
-Set-Location "C:\Projects\B2B-Frontend_JP\B2B-Music-Streaming-Platform\frontend"
-Write-Host "Starting Frontend Dev Server..." -ForegroundColor Cyan
-npm run dev
